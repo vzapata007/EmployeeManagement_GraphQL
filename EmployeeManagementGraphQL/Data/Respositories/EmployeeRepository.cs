1113d6d7fc58e1f7d5cad9738f2a1a356296bd3d
Victor Zapata <victor.zapata@msn.com>
2024-07-17 22:57:23 +1200

    Add project files.

---
+++ EmployeeManagementGraphQL/Data/Respositories/EmployeeRepository.cs
@@ -0,0 +1,73 @@
+﻿using EmployeeManagementGraphQL.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementGraphQL.Data.Repositories
+{
+    // Class to handle CRUD operations for the Employee entity
+    public class EmployeeRepository
+    {
+        // Database context to access the entities
+        private readonly EntityDatabaseContext _context;
+
+        // Constructor that receives the database context
+        public EmployeeRepository(EntityDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // Method to get all employees, including their reviews
+        public List<Employee> GetAllEmployees()
+        {
+            // Include the Reviews entity in the query to load the related reviews for each employee
+            return _context.EmployeeEntity.Include(f => f.Reviews).ToList();
+        }
+
+        // Method to get an employee by their ID, including their reviews
+        public Employee? GetEmployeeById(int id)
+        {
+            // Find the employee by ID and include their reviews in the query
+            return _context.EmployeeEntity.Include(f => f.Reviews).Where(d => d.Id == id).FirstOrDefault();
+        }
+
+        // Method to add a new employee to the database
+        public Employee AddEmployee(Employee employee)
+        {
+            // Add the employee to the EmployeeEntity set
+            _context.EmployeeEntity.Add(employee);
+            // Save the changes to the database
+            _context.SaveChanges();
+            return employee;
+        }
+
+        // Method to update an existing employee in the database
+        public Employee? UpdateEmployee(int id, Employee employee)
+        {
+            // Find the employee by ID
+            var _employee = _context.EmployeeEntity.Where(d => d.Id == id).FirstOrDefault();
+            if (_employee != null)
+            {
+                // Update the employee's fields with the new values
+                _employee.Email = employee.Email;
+                _employee.FirstName = employee.FirstName;
+                _employee.LastName = employee.LastName;
+                // Save the changes to the database
+                _context.SaveChanges();
+            }
+            return _employee;
+        }
+
+        // Method to delete an employee from the database by their ID
+        public void DeleteEmployee(int id)
+        {
+            // Find the employee by ID
+            var _employee = _context.EmployeeEntity.Where(d => d.Id == id).FirstOrDefault();
+            if (_employee != null)
+            {
+                // Remove the employee from the EmployeeEntity set
+                _context.EmployeeEntity.Remove(_employee);
+                // Save the changes to the database
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ EmployeeManagementGraphQL/GraphQL/Mutations/EmployeeMutation.cs
@@ -0,0 +1,77 @@
+﻿using EmployeeManagementGraphQL.Data.Models;
+using EmployeeManagementGraphQL.Data.Repositories;
+using EmployeeManagementGraphQL.GraphQL.Types;
+using GraphQL;
+using GraphQL.Types;
+
+namespace EmployeeManagementGraphQL.GraphQL.Mutations
+{
+    // Class to define mutations for the Employee entity in the GraphQL schema
+    public class EmployeeMutation : ObjectGraphType
+    {
+        // Constructor that sets up the mutation fields
+        public EmployeeMutation(EmployeeRepository repository)
+        {
+            // Field for adding a new employee
+            Field<EmployeeType>(
+                "addEmployee",
+                description: "Is used to add a new employee to the database",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<EmployeeInputType>> { Name = "employee", Description = "Employee input parameter." }
+                ),
+                resolve: context =>
+                {
+                    // Retrieve the employee argument from the context
+                    var employee = context.GetArgument<Employee>("employee");
+                    if (employee != null)
+                    {
+                        // Add the new employee using the repository
+                        return repository.AddEmployee(employee);
+                    }
+                    return null;
+                });
+
+            // Field for updating an existing employee
+            Field<EmployeeType>(
+               "updateEmployee",
+               description: "Is used to update an existing employee in the database",
+               arguments: new QueryArguments(
+                   new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "id", Description = "ID of the employee that needs to be updated" },
+                   new QueryArgument<NonNullGraphType<EmployeeInputType>> { Name = "employee", Description = "Employee input parameter." }
+               ),
+               resolve: context =>
+               {
+                   // Retrieve the ID and employee arguments from the context
+                   var id = context.GetArgument<int>("id");
+                   var employee = context.GetArgument<Employee>("employee");
+                   if (employee != null)
+                   {
+                       // Update the employee using the repository
+                       return repository.UpdateEmployee(id, employee);
+                   }
+                   return null;
+               });
+
+            // Field for deleting an existing employee
+            Field<EmployeeType>(
+              "deleteEmployee",
+              description: "Is used to delete an existing employee from the database",
+              arguments: new QueryArguments(
+                  new QueryArgument<NonNullGraphType<IdGraphType>>
+                  {
+                      Name = "id",
+                      Description = "ID of the employee that needs to be deleted"
+                  }
+              ),
+              resolve: context =>
+              {
+                  // Retrieve the ID argument from the context
+                  var id = context.GetArgument<int>("id");
+                  // Delete the employee using the repository
+                  repository.DeleteEmployee(id);
+                  // Return true to indicate successful deletion
+                  return true;
+              });
+        }
+    }
+}
